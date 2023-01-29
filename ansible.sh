1da9b6c5d139baeeb64a09025527f12b6aaa0953
Thilo Ettelt <thilo.ettelt@neuland-bfi.de>
2023-01-29 17:06:58 +0100

    initial commit

---
+++ ansible.sh
@@ -0,0 +1,8 @@
+sudo apt install -y ansible
+
+ansible-galaxy install -r requirements.yml
+ansible-galaxy collection install -r requirements.yml
+
+ansible-playbook playbook_all.yml -K
+
+#Random order, use with care: ansible-playbook playbook_*.yml -K # reminder: --tags "dconf" # --verbose --tags ""
